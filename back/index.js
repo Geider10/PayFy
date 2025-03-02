cd3a60900d4a3191930a111b54eab4096c03229d
Geider Frias <geiderde20@gmail.com>
2025-03-02 16:09:03 -0300

    update index and create env.example

--- back/index.js
+++ back/index.js
@@ -2,8 +2,12 @@ import {PORT} from './src/utils/config.js';
 import {connectDB} from './src/utils/db.js';  
 import {app} from './app.js';
 
-connectDB()
-.then(()=> {
-    console.log('Conection with DB');
-    app.listen(PORT,'0.0.0.0', ()=>{console.log(`Server running in port ${PORT}`);})
-}).catch(() => console.log('Error to connect DB'))
+function main (){
+    connectDB()
+    .then(()=> {
+        console.log('Conection with DB');
+        app.listen(PORT,'0.0.0.0', ()=>{console.log(`Server running in port ${PORT}`);})
+    }).catch(() => console.log('Error to connect DB'))
+}
+
+main();
