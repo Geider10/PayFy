8fc9c3554b37e92d744236c499c5317d76b492f2
Geider Frias <geiderde20@gmail.com>
2025-03-02 22:02:30 -0300

    add morgan for show apis and add rate-limit for avoid attack ddos

--- back/app.js
+++ back/app.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import morgan from 'morgan';
+import {limiter} from './src/utils/config.js';
 import {validateToken} from './src/middlewares/validateToken.js';
 import {errorMiddleware} from './src/middlewares/error.js';
 
@@ -10,8 +12,11 @@ import {authRouter} from './src/routers/auth.router.js';
 import {providerRouter} from './src/routers/provider.router.js';
 
 export const app = express();
+//middlewars keys
 app.use(cors({origin : '*'}))
-app.use(express.json());
+app.use(express.json())
+app.use(limiter)
+app.use(morgan('dev'))
 //routes public access 
 app.use('/auth',authRouter)
 //routes protected by token
@@ -20,5 +25,6 @@ app.use('/provider', providerRouter)
 app.use('/user',userRouter)
 app.use('/payment',paymentRouter)
 app.use('/card',cardRouter)
+//middlewars errors
 app.use(errorMiddleware)
 
